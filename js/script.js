318f7f3d3d22dd0d31e855d510d40aefea8c9a21
RoshanA <Roshan.Abraham@gmail.com>
2016-03-24 19:44:53 -0400

    My changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,53 @@
+var submit = document.getElementById('submit');
+
+submit.onclick = tipCalculate;
+
+
+
+
 function tipCalculate(){
   console.log("TIP CALCULATE");
   //the function that will run 
   //when the submit button is clicked
   //as defined in the inline javascript
   //on index.html
+var totalBill = document.getElementById('bill').value;
+
+console.log(totalBill);
+
+if(!isNaN(totalBill)){ 
+
+var tipAmount = (totalBill * tipPerc * 100);
+
+var roundedamount =tipAmount.toFixed(2);
+
+var tipBox = document.getElementById('tipamount');
+tipBox.innerHTML = $ +roundedamount; 
+
+function getPercChecked()
+
+} else {alert("please enter numbers only")}
+
+{
+
+
+var tipBtns = document.getElementsByClassName('tip-btn');
+
+for (var i=0 ; i < percBtns.length ; i++)
+
+console.log(percBtns[i].checked);
+
+if(percBtns[i].checked) {
+
+
+	return percBtns[i].value;
+
 }
+
+
+}
+
+
+
+
+
